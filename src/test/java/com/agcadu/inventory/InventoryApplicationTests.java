75d8d432643d7903377f4700d1e367687ad28068
Agustin <agcadu@gmail.com>
2023-05-25 18:10:14 +0200

    primer commit

---
+++ src/test/java/com/agcadu/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agcadu.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
